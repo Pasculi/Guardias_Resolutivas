85645bc83c091e7385651b60bb41a02d94b472e6
Pasculi <jorge.sepulveda.turpie@gmail.com>
2023-04-04 22:53:03 -0400

    feat:Se agrega Modal de editar

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
 const formulario = document.querySelector('.formulario');
-const btnEdit = document.querySelector('.header__btn-edit')
+const btnEdit = document.querySelector('.header__btn-edit');
+const btnCerrar = document.querySelector('.btn__cerrar')
 
 function editGuardias(){
     formulario.style.display = 'flex';
 }
 btnEdit.addEventListener('click', editGuardias);
+
+function cerrarGuardias(){
+    formulario.style.display = 'none';
+}
+btnCerrar.addEventListener('click', cerrarGuardias);
