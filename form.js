b30ef11781d97333e1d8b45ddf16854e619abe58
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2023-04-11 22:23:21 -0400

    Revisar ejercicios

--- form.js
+++ form.js
@@ -1,8 +1,141 @@
-function pinchame(){
-    alert('Desde archivo externo')
-}
-document.getElementById('pinchable2').onclick = pinchame;
-const nombre = document.querySelector('#nombre');
+/* const people = [
+    "Steven Spielberg",
+    "Michael Bay",
+    "Robin Spielberg",
+    "Sasha Rebecca Spielberg",
+    "James Cameron"
+]; */
+
+// Crea un nuevo array vacío en una variable spielbergs
+/* const spielbergs = []
+people.forEach(function (element) {
+    if (element.includes("Spielberg")) {
+        spielbergs.push(element);
+    }
+  
+});
+console.log(spielbergs); */
+// Muestra el array spielbergs resultante en la consola.
+
+/* const words = [
+    "Baden",
+    "aye",
+    "go",
+    "agar"
+];
+
+const reduplications = words.map(function (word) {
+    return `${word}-${word}`;
+})  */// Continúa con el código aquí
+
+/* console.log(reduplications);
+
+const counter = [
+    "Emperador",
+    "Rey",
+    "César",
+    "Faraón",
+    "Sultán",
+    "Zar"
+];
+
+const counterIndexed = counter.map(function (person, index) {
+    return `${index + 1}. ${person}`;
+});
+
+console.log(counterIndexed);
+
+const a = [
+    "Harry Potter y la piedra filosofal",
+    "Harry Potter y la cámara secreta",
+    "Harry Potter y el prisionero de Azkaban",
+    "Harry Potter y el cáliz de fuego",
+    "Harry Potter y la Orden del Fénix",
+    "Harry Potter y el misterio del príncipe",
+    "Harry Potter y las Reliquias de la Muerte",
+    "Harry Potter y los métodos de racionalidad"
+];
+
+const b = [];
+a.forEach(function (item, i, a) {
+    
+    b.push(a[i].split(" y ")[1]);
+}) 
 
 
+console.log(b);
 
+const arr = [
+    "Interacción gravitatoria",
+    "Fuerza electromagnética",
+    "Fuerza nuclear fuerte",
+    "Fuerza nuclear débil"
+];
+
+arr.forEach(function (element, index) {
+    console.log(`${index + 1}. ${element}`)
+});
+ */
+
+/* const characters = [
+    "Luke Skywalker",
+    "Obi-Wan",
+    "Chewbacca",
+    "Anakin Skywalker",
+    "Han Solo",
+    "Palpatine"
+];
+
+const newCharacters = characters.map(function (character) {
+    if (character === "Anakin Skywalker") {
+        return "Darth Vader";
+    } else {
+        return character;
+    }
+});
+
+console.log(newCharacters); */
+
+/* const movies = [
+    "Titanic (1997)",
+    "Black Panther (2018)",
+    "Isle of Dogs (2018)",
+    "The Hateful Eight (2015)"
+];
+
+const moviesFiltered = movies.filter(function (item) {
+    return item.includes("2018");
+});
+
+console.log(moviesFiltered); */
+
+const cards = [
+    "6 de corazones", "7 de corazones", "8 de corazones",
+    "9 de corazones", "10 de corazones", "Jota de corazones",
+    "Reina de corazones", "Rey de corazones",
+    "As de corazones", "6 de picas", "7 de picas",
+    "8 de picas", "9 de picas", "10 de picas",
+    "Jota de picas", "Reina de picas",
+    "Rey de picas", "As de picas", "6 de tréboles",
+    "7 de tréboles", "8 de tréboles", "9 de tréboles",
+    "10 de tréboles", "Jota de tréboles", "Reina de tréboles",
+    "Rey de tréboles", "As de tréboles", "6 de diamantes",
+    "7 de diamantes", "8 de diamantes", "9 de diamantes",
+    "10 de diamantes", "Jota de diamantes",
+    "Reina de diamantes", "Rey de diamantes",
+    "As de diamantes"
+];
+
+/* const cardsFiltered = cards.filter(function (card) { */
+    /* Escribe el código aquí */
+/*     return card.includes("10") && (card.includes("corazones") || card.includes("diamantes"));
+}) */;
+
+const cardsFiltered = cards.filter(function (card) {
+    return parseInt(card, 10) <= 10
+        && (card.includes("de corazones")
+            || card.includes("de diamantes"));
+});
+
+
+console.log(cardsFiltered);
--- js/index.js
+++ js/index.js
@@ -3,50 +3,50 @@ const btnEdit = document.querySelector(".header__btn-edit");
 const btnCerrar = document.querySelector(".formulario__btn-cerrar");
 
 function editGuardias() {
-  formulario.style.display = "flex";
+    formulario.style.display = "flex";
 }
 btnEdit.addEventListener("click", editGuardias);
 
 function cerrarGuardias() {
-  formulario.style.display = "none";
+    formulario.style.display = "none";
 }
 btnCerrar.addEventListener("click", cerrarGuardias);
 
 const tipoGuardia = [
-  { id: 1, nombre: "Guardia Subtel" },
-  { id: 2, nombre: "Guardia B" },
-  { id: 3, nombre: "Guardia C" },
+    { id: 1, nombre: "Guardia Subtel" },
+    { id: 2, nombre: "Guardia B" },
+    { id: 3, nombre: "Guardia C" },
 ];
 
 const personas = [
-  { guardiaId: 1, id: 1, nombre: "Raul Caulier", telefono: "+56967605441" },
-  { guardiaId: 1, id: 2, nombre: "Manuel Araneda", telefono: "+56967603409" },
-  { guardiaId: 1, id: 3, nombre: "Patricio Ortega", telefono: "+5697391120" },
-  { guardiaId: 1, id: 4, nombre: "Erwin Eggers", telefono: "+56983410816" },
-  { guardiaId: 1, id: 5, nombre: "Daniel Cayuñir", telefono: "+56988888888" },
-  { guardiaId: 2, id: 6, nombre: "Andrea Burgos", telefono: "+56967603275" },
-  { guardiaId: 2, id: 7, nombre: "Marcelo Sandoval", telefono: "+56997391027" },
-  { guardiaId: 2, id: 8, nombre: "Mario Colihueque", telefono: "+56967604831" },
-  { guardiaId: 2, id: 3, nombre: "Patricio Ortega", telefono: "+5697391120" },
-  { guardiaId: 3, id: 9, nombre: "Rodrigo Soriano", telefono: "+56982661596" },
-  { guardiaId: 3, id: 8, nombre: "Mario Colihueque", telefono: "+56967604831" },
-  { guardiaId: 3, id: 10, nombre: "Oscar Vivar", telefono: "+5697391175" },
-  { guardiaId: 3, id: 11, nombre: "Ricardo Riebel", telefono: "+5698282252" },
-  { guardiaId: 3, id: 3, nombre: "Patricio Ortega", telefono: "+5697391120" },
-  { guardiaId: 3, id: 7, nombre: "Marcelo Sandoval", telefono: "+56997391027" },
+    { guardiaId: 1, id: 1, nombre: "Raul Caulier", telefono: "+56967605441" },
+    { guardiaId: 1, id: 2, nombre: "Manuel Araneda", telefono: "+56967603409" },
+    { guardiaId: 1, id: 3, nombre: "Patricio Ortega", telefono: "+5697391120" },
+    { guardiaId: 1, id: 4, nombre: "Erwin Eggers", telefono: "+56983410816" },
+    { guardiaId: 1, id: 5, nombre: "Daniel Cayuñir", telefono: "+56988888888" },
+    { guardiaId: 2, id: 6, nombre: "Andrea Burgos", telefono: "+56967603275" },
+    { guardiaId: 2, id: 7, nombre: "Marcelo Sandoval", telefono: "+56997391027" },
+    { guardiaId: 2, id: 8, nombre: "Mario Colihueque", telefono: "+56967604831" },
+    { guardiaId: 2, id: 3, nombre: "Patricio Ortega", telefono: "+5697391120" },
+    { guardiaId: 3, id: 9, nombre: "Rodrigo Soriano", telefono: "+56982661596" },
+    { guardiaId: 3, id: 8, nombre: "Mario Colihueque", telefono: "+56967604831" },
+    { guardiaId: 3, id: 10, nombre: "Oscar Vivar", telefono: "+5697391175" },
+    { guardiaId: 3, id: 11, nombre: "Ricardo Riebel", telefono: "+5698282252" },
+    { guardiaId: 3, id: 3, nombre: "Patricio Ortega", telefono: "+5697391120" },
+    { guardiaId: 3, id: 7, nombre: "Marcelo Sandoval", telefono: "+56997391027" },
 ];
 
 const selectGuardia = document.querySelector("#tipo_guardia");
 const nombreGuardia = document.querySelector("#nombre_guardia");
 
 const tipoGuard = tipoGuardia.map((item) => {
-  const optionElement = document.createElement("option");
-  optionElement.classList.add("tipo_guardia");
-  optionElement.value = item.id;
-  optionElement.text = item.nombre;
-  selectGuardia.appendChild(optionElement);
+    const optionElement = document.createElement("option");
+    optionElement.classList.add("tipo_guardia");
+    optionElement.value = item.id;
+    optionElement.text = item.nombre;
+    selectGuardia.appendChild(optionElement);
 
-  /* console.log(item); */
+    /* console.log(item); */
 });
 /*Seleccionar tipo de guardia, para llenar select con nombres */
 
@@ -56,27 +56,31 @@ const optionGuardia = document.querySelector("#tipo_guardia");
 
 function optionGuard(e) {
 
-  const optionSelect = optionGuardia.options[optionGuardia.selectedIndex];
-  console.log(optionSelect);
-  const valor = parseInt(optionSelect.value);
-  console.log(valor);
+    const optionSelect = optionGuardia.options[optionGuardia.selectedIndex];
+    console.log(optionSelect);
+    const valor = parseInt(optionSelect.value);
 
-  /*Funcion donde creamos el filtro para ser mostrado en el select de name__Guardia */
+    console.log(valor);
+    const data = valor;
+    console.log("Estos es igual a valor=" + data);
 
-  function filtrarPersonas(valor) {
-    const nameGuard = personas.filter((persona) => persona.guardiaId === valor);
-    /* console.log(typeof nameGuard); */
-    nameGuard.map((element) => {
-      const optionName = document.createElement("option");
-      optionName.classList.add("name_guardia");
-      optionName.value = element.id;
-      optionName.text = element.nombre;
-      nombreGuardia.appendChild(optionName);
-    });
-  }
-  filtrarPersonas(valor);
+    /*Funcion donde creamos el filtro para ser mostrado en el select de name__Guardia */
+
+    function filtrarPersonas(data) {
+        const nameGuard = personas.filter((persona) => persona.guardiaId === data);
+        /* console.log(typeof nameGuard); */
+        nameGuard.map((element) => {
+            const optionName = document.createElement("option");
+            optionName.classList.add("name_guardia");
+            optionName.value = element.id;
+            optionName.text = element.nombre;
+            nombreGuardia.appendChild(optionName);
+        });
+    }
+    filtrarPersonas(data);
 }
 selectGuardia.addEventListener("change", optionGuard);
+optionGuardia = "0";
 
 /*Limpiar select */
 
@@ -85,18 +89,18 @@ selectGuardia.addEventListener("change", optionGuard);
 
 
 const nombre = document.querySelector("#nombre_guardia");
-function optionNombre(e) {
-  const optionSelect = nombre.options[nombre.selectedIndex];
-  console.log(optionSelect.text);
-  console.log(optionSelect.value);
+function optionNombre() {
+    const optionSelect = nombre.options[nombre.selectedIndex];
+    console.log(optionSelect.text);
+    console.log(optionSelect.value);
 }
 nombre.addEventListener("change", optionNombre);
 
 const btn_cambiarGuardia = document.querySelector(".formulario__btn");
 
 function cambiarGuardia(e) {
-  e.preventDefault();
-  const seleccion = selectGuardia.value;
-  console.log(seleccion);
+    e.preventDefault();
+    const seleccion = selectGuardia.value;
+    console.log(seleccion);
 }
 btn_cambiarGuardia.addEventListener("submit", cambiarGuardia);
