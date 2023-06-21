6a2e1bfa82644def413b24086fe2e4f88460c528
Jorge R. Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2023-06-20 22:39:48 -0400

    feat: Se habilita localStorage 2

--- js/localstorage.js
+++
@@ -1,8 +0,0 @@
-/* const guardSub = document.querySelector('.card__nombre_Sub');
-const guardSubPhone = document.querySelector('.card__phone-number_Sub');
-const guardB = document.querySelector('.card__nombre_B');
-const guardBPhone = document.querySelector('.card__phone-number_B');
-const guardC = document.querySelector('.card__nombre_C');
-const guardCPhone = document.querySelector('.card__phone-number_C'); */
-
-
