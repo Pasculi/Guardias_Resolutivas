084ee1c0fd387d3f75ff1c1a8967d874373cf8f8
Pasculi <jorge.sepulveda.turpie@gmail.com>
2023-07-13 08:18:45 -0400

    feat:Se habilita el refresco de la página cada 12hrs.

--- js/index.js
+++ js/index.js
@@ -3,6 +3,13 @@ document.addEventListener("DOMContentLoaded", () => {
   leerLocalStorage();
  });
 
+// Función actualizar
+function actualizar() {
+  location.reload(true)
+}
+// Función para actualizar cada 4 segundos(4000 milisegundos)
+setInterval("actualizar()", 61200000);
+
 const formulario = document.querySelector(".formulario");
 const btnEdit = document.querySelector(".header__btn-edit");
 const btnCerrar = document.querySelector(".formulario__btn-cerrar");
